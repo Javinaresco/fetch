abedd1747ed5cd6a189526f9d93bc727b6643cd4
Francisco Javier Naranjo Escobio <franciscojaviernaranjoescobio@alumno.ieselrincon.es>
2025-02-14 12:54:18 +0000

    xd

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "../css/chatbox.css";
+import "../css/Chatbox.css";
 
 export const Chatbot = () => {
   const [messages, setMessages] = useState([
